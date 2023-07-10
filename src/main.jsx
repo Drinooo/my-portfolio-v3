72ac7c747aa4e47f035010edde2dd5691198e79a
NMS Design <nmsdesign@AldrinITD.local>
2023-07-10 16:15:50 +0800

    Preloader activated

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <ThemeProvider>
       <BrowserRouter>
         <App />
-        {/* <Sugar background="#151516" color="#FFF" time={3000} /> */}
+        <Sugar background="#151516" color="#FFF" time={3000} />
       </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>
