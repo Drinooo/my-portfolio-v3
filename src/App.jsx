2553ced9ae7690e721f7b93eeb68e87d2eac3ca1
NMS Design <nmsdesign@AldrinITD.local>
2023-07-10 13:34:56 +0800

    Added - React Preloader

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,15 @@ import { NavbarComponent } from "./components/NavbarComponent";
 import { FooterComponent } from "./components/FooterComponent";
 import { HeroSection } from "./sections/HeroSection";
 import { ParallaxProvider } from "react-scroll-parallax";
+import { Lines, Sugar } from "react-preloaders";
 
 function App() {
   return (
     <>
-      <ParallaxProvider>
-        <NavbarComponent />
-        <HeroSection />
-        <FooterComponent />
-      </ParallaxProvider>
+      <Sugar background="#151516" color="#FFF" time={3000} />
+      <NavbarComponent />
+      <HeroSection />
+      <FooterComponent />
     </>
   );
 }
